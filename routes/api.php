90deb4894894c6762e5b6fd5f9e7abd7b60f4cc5
Naydenov,Viktor V <v.naydenov@student.fontys.nl>
2020-10-05 23:05:51 +0200

    established user authentication using passport

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+
+    // ...
+
+    // public routes
+    //THIS IS THE WAY OF WRITING THE ROUTES IN LARAVEL 8 (LIKE AN ARRAY)!!!
+    Route::post('/login', [\App\Http\Controllers\Auth\ApiAuthController::class, 'login'])->name('login.api');
+    Route::post('/register', [\App\Http\Controllers\Auth\ApiAuthController::class, 'register'])->name('register.api');
+
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Auth\ApiAuthController::class, 'register'])->name('logout.api');
+});
