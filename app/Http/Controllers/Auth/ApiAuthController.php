9b9c9fdf68e3f7dff6bca8ce6f9d36ac6a72f4bd
nevski09 <nevski09@students.softuni.bg>
2020-10-06 10:38:54 +0200

    added email and changed name to username in mig/mod

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -13,7 +13,7 @@ class ApiAuthController extends Controller
 {
     public function register (Request $request) {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
