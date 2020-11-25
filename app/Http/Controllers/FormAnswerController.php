53d1c03e01afe8f2eb3e722d6465c537e4884b2e
Gekov,Aleksandar A.O <a.gekov@student.fontys.nl>
2020-11-25 01:55:08 +0100

    added search json answers by parameter, default is name

--- app/Http/Controllers/FormAnswerController.php
+++ app/Http/Controllers/FormAnswerController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\FormAnswer;
 use Illuminate\Http\Request;
-use PhpParser\Node\Scalar\String_;
 
 class FormAnswerController extends Controller
 {
@@ -12,16 +11,16 @@ class FormAnswerController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
 
         $formanswer = new FormAnswer();
-        $formanswer->company_id=$request->company_id;
-        $formanswer->visitor_id=$request->visitor_id;
-        $formanswer->answers=json_encode($request->answers);
+        $formanswer->company_id = $request->company_id;
+        $formanswer->visitor_id = $request->visitor_id;
+        $formanswer->answers = json_encode($request->answers);
         $formanswer->save();
         return response()->json([
             'message' => 'success'
@@ -29,55 +28,35 @@ class FormAnswerController extends Controller
 
     }
 
-    public function get(Request $request) {
+    public function get(Request $request)
+    {
         return FormAnswer::where('company_id', $request->company_id)->where('visitor_id', $request->visitor_id)->get();
     }
 
-    public function getById(Request $request) {
+    public function getById(Request $request)
+    {
         return FormAnswer::where('id', $request->id)->get();
     }
 
-    public function getData(Request $request, String $param) {
+    /**
+     * Get data.
+     *
+     * @param Request $request
+     * @param string $company_name
+     * @param string $param
+     * @return String
+     */
+    public function getData(Request $request, $name, string $param = null)
+    {
 
         $answers = FormAnswer::where('company_id', $request->company_id)->where('visitor_id', $request->visitor_id)->first();
         $data = json_decode($answers->answers);
 
-        //If does not work add ''
-        $asd = 'name';
-        if ($param == String_::class){
-            $asd = $param;
+
+        if ($param == '') {
+            return $data->{'name'};
         }
-        return $data->{$asd};
-
-    }
-    public function getDateStart(Request $request) {
-
-        $answers = FormAnswer::where('company_id', $request->company_id)->where('visitor_id', $request->visitor_id)->first();
-        $data = json_decode($answers->answers);
-
-        //If does not work add ''
-
-        return $data->{'date-start'};
-
-    }
-    public function getIsLonger(Request $request) {
-
-        $answers = FormAnswer::where('company_id', $request->company_id)->where('visitor_id', $request->visitor_id)->first();
-        $data = json_decode($answers->answers);
-
-        //If does not work add ''
-
-        return $data->{'isLonger'};
-
-    }
-    public function getDateEnd(Request $request) {
-
-        $answers = FormAnswer::where('company_id', $request->company_id)->where('visitor_id', $request->visitor_id)->first();
-        $data = json_decode($answers->answers);
-
-        //If does not work add ''
-
-        return $data->{'date-end'};
+        return $data->{$param};
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,8 +29,7 @@ Route::group(['middleware' => ['json.response']], function () {
     Route::get('/companies/{company_id}/form/answers', [\App\Http\Controllers\FormAnswerController::class, 'get']);
     Route::get('/companies/{company_id}/form/answers', [\App\Http\Controllers\FormAnswerController::class, 'getById']);
     Route::post('/companies/{company_id}/form/answers', [\App\Http\Controllers\FormAnswerController::class, 'store']);
-    Route::get('/companies/{company_id}/form/answers/getData/{param}', [\App\Http\Controllers\FormAnswerController::class, 'getData']);
-    Route::get('/companies/{company_id}/form/answers/getData', [\App\Http\Controllers\FormAnswerController::class, 'getData']);
+    Route::get('/companies/{company_id}/form/answers/getData/{param?}', [\App\Http\Controllers\FormAnswerController::class, 'getData']);
 });
 
 Route::middleware('auth:api')->group(function () {
